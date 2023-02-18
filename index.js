aa3ae87aad96e1b3148c233b972620dbdb8ddfce
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-18 12:30:28 +0200

    fix code

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 //Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
-let agee = prompt("Сколько вам лет?");
-let ageBad = 17;
-function age(a, b) {
-    return a > b ? true : 'Батьки дозволили?';
+/*let a = prompt("Введите число", '');
+let b = prompt("Еще одно", '');
+function min(a, b) {
+    return a < b ? a : b;
 }
-alert(age(agee, ageBad))
-
+alert(min(a, b));
 
 //Напишіть функцію pow(x, n), яка повертає число x в n степені
 let x = prompt("x?", '');
 let n = prompt("n?", '');
 let subscribe = false;
 
-function pow(x, n) 
-{
-    let result = x;
-    for (let i = 1; i < n; i++) {
-        result *= x;
-        return result;
+function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+      result *= x;
     }
-    if (n < 1 || n === "number")  {
-        alert( pow(x, n) );
-    } else {
-        alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-    }
-    
-} 
-
+    return result;
+}
 alert(pow(x, n));
+*/
+
+//Перепишіть функцію, використовуючи  1) '?'  2) '||' або '&&'
+let agee = prompt("Сколько вам лет?");
+let ageBad = 17;
+function age(a, b) {
+    return a > b || 'Батьки дозволили?';
+}
+alert(age(agee, ageBad));
