9f796a0121418f3bb302e3af28d92880436c52e3
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-03-02 10:52:39 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
-let a = prompt("Введите число", '');
+/*let a = prompt("Введите число", '');
 let b = prompt("Еще одно", '');
 function min(a, b) {
     return a < b ? a : b;
@@ -20,11 +20,48 @@ function pow(x, n) {
 }
 alert(pow(x, n));
 
-
 //Перепишіть функцію, використовуючи  1) '?'  2) '||' або '&&'
-let agee = prompt("Сколько вам лет?");
-let ageBad = 17;
-function age(a, b) {
-    return a > b || 'Батьки дозволили?';
+
+let agee = +prompt("Сколько вам лет?");
+
+function checkAge(age) {
+    return (age >= 18) || confirm('Батьки дозволили?');
 }
-alert(age(agee, ageBad));
+checkAge(agee);
+
+
+function checkAge(age) {
+    return (age > 18) || confirm('Батьки дозволили?');
+}
+alert(checkAge2(8));
+alert(checkAge2(18));
+alert(checkAge2(20));
+*/
+
+let data = {
+    Риба: {
+      форель: {},
+      лосось: {}
+    },
+  
+    Дерево: {
+        яблуня: {},
+        магнолія: {}
+    }
+}
+
+const elem = document.querySelector('div');
+
+for (let key in data) {
+    let ul = document.createElement('ul');
+    ul.innerHTML = key;
+    let childUl = ul.cloneNode(false);
+    ul.append(childUl);
+    
+    for(let el in data[key]) {
+        let li = document.createElement('li');
+        li.innerHTML = el;
+        childUl.append(li);
+    }
+    elem.append(ul);
+}
